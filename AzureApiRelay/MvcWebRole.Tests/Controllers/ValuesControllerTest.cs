a5311321164f32bfc0b6464940c3dafcf8c45ca2
Johan Dewe <johan@dewe.net>
2013-10-03 22:10:33 +0200

    API route wildcard with default controller.
    Clean up: remove unused MVC stuff.

--- AzureApiRelay/MvcWebRole.Tests/Controllers/ValuesControllerTest.cs
+++ AzureApiRelay/MvcWebRole.Tests/Controllers/ValuesControllerTest.cs
@@ -16,66 +16,13 @@ namespace MvcWebRole.Tests.Controllers
         [TestMethod]
         public void Get()
         {
-            // Arrange
-            ValuesController controller = new ValuesController();
+            var controller = new RelayController();
+            var path = "dummy_path";
+            var expected = "get: " + path;
 
-            // Act
-            IEnumerable<string> result = controller.Get();
+            string result = controller.Get(path);
 
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(2, result.Count());
-            Assert.AreEqual("value1", result.ElementAt(0));
-            Assert.AreEqual("value2", result.ElementAt(1));
-        }
-
-        [TestMethod]
-        public void GetById()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
-
-            // Act
-            string result = controller.Get(5);
-
-            // Assert
-            Assert.AreEqual("value", result);
-        }
-
-        [TestMethod]
-        public void Post()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
-
-            // Act
-            controller.Post("value");
-
-            // Assert
-        }
-
-        [TestMethod]
-        public void Put()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
-
-            // Act
-            controller.Put(5, "value");
-
-            // Assert
-        }
-
-        [TestMethod]
-        public void Delete()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
-
-            // Act
-            controller.Delete(5);
-
-            // Assert
+            Assert.AreEqual(expected, result);
         }
     }
 }
---
+++ AzureApiRelay/MvcWebRole/Controllers/RelayController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace MvcWebRole.Controllers
+{
+    public class RelayController : ApiController
+    {
+        // GET api/values
+        public string Get(string path)
+        {
+            return "get: " + path;
+        }
+   }
+}
